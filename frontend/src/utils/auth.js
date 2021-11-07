fe638b9bd00a89c7cc749feafec073633d06f5c3
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-11-08 00:46:51 +0500

    fix
    исправление доменных имен

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.place-tmn.students.nomoredomains.work";
 
 // Проверка ответа =========================================================
 export const checkRespons = (res) => {
